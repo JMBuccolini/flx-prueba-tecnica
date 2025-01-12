b4eb55393d69d4412f8ebc33cb2f610951737733
JMBuccolini <juanmateobuccolini@gmail.com>
2025-01-12 15:28:40 -0300

    ej.1 y 3

--- docs/consultas.sql
+++ docs/consultas.sql
@@ -1,1 +1,14 @@
 -- En este archivo deben estar tus ejercicios de consultas sql
+
+-- 1
+
+SELECT nombre
+FROM Empleados
+ORDER BY nombre DESC;
+
+
+--3
+
+SELECT nombre
+FROM Empleados
+WHERE nombre LIKE '%o';
